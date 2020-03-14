fe12bcaad43c4a9a8ff6d9382b9d99f66d2de4cb
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-03-14 19:56:37 +0545

    App.js

--- my_path_to_react/src/App.js
+++ my_path_to_react/src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Navbar from './components/navbar';
 import UserProfile from './components/UserProfile';
 
-import './'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Todolist from './components/TodoComponents/Todolist';
+import Todo from './components/TodoComponents/Todo';
+import TodoForm from './components/TodoComponents/TodoForm';
+
+
 function App() {
   return (
-    <div className= "navbar">
-      <Navbar />
-      <div className="userProfile center">
-        <UserProfile />
+    <div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="container  ">
+        <div className="row mt-5">
+
+          <div className="col-md-4">
+            <div className="card  p-3 text-center"><strong className="">Mero Profile</strong>
+              <UserProfile />
+            </div>
+          </div>
+
+          <div className="col-md-8">
+            <div className="card p-3">
+            <h2 className="text-center btn  text-dark mb-3">Todo App</h2>
+            <Todolist /></div>
+          </div>
+
+        </div>
       </div>
     </div>
-   
+
   );
 }
 
