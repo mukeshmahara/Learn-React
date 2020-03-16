04519faa255c5c5bb2d737939e1124dc97d38db6
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-03-16 13:16:34 +0545

    form data service

---
+++ my_path_to_react/src/components/LoginComponent/services/PostData.js
@@ -0,0 +1,19 @@
+const PostData = (type, userData) => {
+    let BaseUrl = 'https://api.thewallscript.com/restful';
+    return new Promise((resolve, reject) => {
+        fetch(BaseUrl + type, {
+            method: 'POST',
+            body: JSON.stringify(userData)
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                resolve(responseJson);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+    });
+
+}
+
+export default PostData;
