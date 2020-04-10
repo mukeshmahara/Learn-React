a85a2edd5e59debb6a5b516daf8553eeca679d99
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-04-10 11:05:46 +0545

    Server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const app = require('./app');
 
+const port = process.env.PORT ||4000;
 
-const port = process.env.PORT ||3000;
 
-const server = http.createServer();
+
+
+const server = http.createServer(app);
 
 server.listen(port);
